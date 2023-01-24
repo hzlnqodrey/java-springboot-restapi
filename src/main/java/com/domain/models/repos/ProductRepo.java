d3b7f40f10a2557020c997d008b8c614a1bcd55d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 17:13:32 +0700

    add product business logic in ProductService.java and add some infos about other repositores in repo files

--- src/main/java/com/domain/models/repos/ProductRepo.java
+++ src/main/java/com/domain/models/repos/ProductRepo.java
@@ -2,6 +2,14 @@ package com.domain.models.repos;
 
 // DAO [Data Access Object ->  is a object/interface, which is used to access data from database of data storage.]
 
+// Selain Crud Repositor, spring java juga memiliki:
+// 1. PagingAndSortingRepository Repository [ref: https://www.baeldung.com/spring-data-jpa-pagination-sorting]
+// guna: Pagination dan Sorting SpringData
+
+// 2. JPA Repository
+// guna: dapat mewariskan paging and sorting repo + ada fitur2 tambahan 
+// misalnya melakukan update secara batching, dan save/create secara bulk/batching
+
 import org.springframework.data.repository.CrudRepository; // extends Interface repository yg dimiliki oleh Java
 import com.domain.models.entities.Product; // import Product model/entities
 
---
+++ src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,46 @@
+package com.domain.services;
+// CLASS SERVICE
+// biasanya di apps complex, class service berisi Business Logic
+// misalnya:
+// 1. Business Process untuk Transfer [Rekening A -> Rekening B]
+// nah flow untuk mengimplement transfer itu akan ada di class2 service
+// akan dicek data transaksi, cek tabel, cek field, cek saldo dkk (gitulah pokoknya)
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.domain.models.entities.Product;
+import com.domain.models.repos.ProductRepo;
+
+@Service
+@Transactional
+public class ProductService {
+    
+    // Inject ProductService into ProductRepo
+    @Autowired // mengasih dependensi injection
+    private ProductRepo productRepo;
+
+    // Simpan/Create Product
+    public Product save(Product product) {
+        return productRepo.save(product);
+    }
+
+    // Find One (Read One Product by ID)
+    public Product findOne(Long id) {
+        return productRepo.findById(id).get();
+    }
+
+    // Find All (Read All Products)
+    public Iterable<Product> findAll() {
+        return productRepo.findAll();
+    }
+
+    // Remove One (Delete One Product by ID)
+    public void removeOne(Long id) {
+        productRepo.deleteById(id);
+    }
+
+
+}
