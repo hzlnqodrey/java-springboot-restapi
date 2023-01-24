d3c208a10bebc1cf9882903324f1ee82fbb85bb2
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 15:30:14 +0700

    add default setter getter for Product class

--- src/main/java/com/domain/models/entities/Product.java
+++ src/main/java/com/domain/models/entities/Product.java
@@ -33,7 +33,38 @@ public class Product implements Serializable {
         this.price = price;
     }
 
+
     // SETTER GETTER
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
 }
