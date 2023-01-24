658b721ac1cb954d41476296151065c48eed78fc
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 15:05:14 +0700

    add MySQL driver and JPA ORM in dependencies

--- src/main/java/com/domain/controllers/WelcomeController.java
+++ src/main/java/com/domain/controllers/WelcomeController.java
@@ -1,7 +1,6 @@
 package com.domain.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping; // GET METHOD
-import org.springframework.web.bind.annotation.PostMapping; // POST METHOD
 import org.springframework.web.bind.annotation.RequestMapping; // for URL MAPPING / ENDPOINT
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +11,6 @@ public class WelcomeController {
     @GetMapping
     public String welcome(){
         return "Welcome to SpringBoot REST API";
-    }
-
-    @PostMapping
-    public String other(){
-        return "Data lain";
     }
 
 }
