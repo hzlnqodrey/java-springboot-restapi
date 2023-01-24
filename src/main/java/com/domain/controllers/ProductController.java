f42fdc1f44573897cdde3adc4ad9549d6a35bd37
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 18:16:45 +0700

    implement update controller

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,19 +28,30 @@ public class ProductController {
     // Argumen dalam BODY REQUEST, kasih anotasi request body ke params
     @PostMapping
     public Product create(@RequestBody Product product) {
+        System.out.println("Creating product...");
         return productService.save(product);
     }
 
     //=== 2. Find All Products
     @GetMapping
     public Iterable<Product> findAll() {
+        System.out.println("Fetching all products...");
         return productService.findAll();
     }
 
     //=== 3. Find One Product by Id
     @GetMapping("/{id}")
     public Product findOne(@PathVariable("id") Long id) {
+        System.out.println("Fetching product...");
         return productService.findOne(id);
     }
 
+    //=== 4. Update One Produdct by Id
+    @PutMapping
+    public Product update(@RequestBody Product product) {
+        System.out.println("Updating product...");
+        return productService.save(product);
+    }
+
+    
 }
