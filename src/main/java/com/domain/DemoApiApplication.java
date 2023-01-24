8e63416cbe73c933a9dfe6030f32b8567fd97754
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 14:45:18 +0700

    Request Mapping: Index Route of /api/welcome

--- src/main/java/com/domain/DemoApiApplication.java
+++ src/main/java/com/domain/DemoApiApplication.java
@@ -1,4 +1,4 @@
-package com.domain.demoapi;
+package com.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/domain/controllers/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.domain.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping; // GET METHOD
+import org.springframework.web.bind.annotation.RequestMapping; // for URL MAPPING / ENDPOINT
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to SpringBoot REST API";
+    }
+
+}
