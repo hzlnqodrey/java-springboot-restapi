c96a229afa1f81e96d3adc639076bc17412b21bd
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 17:36:36 +0700

    create create and read all controller

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package com.domain.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +20,19 @@ public class ProductController {
     @Autowired 
     private ProductService productService;
 
-    // ENDPOINT PRODUCT
+    //=== ENDPOINT PRODUCT
 
-    public Product create(Product product) {
+    //=== 1. Save/Insert/Create Product
+    // Argumen dalam BODY REQUEST, kasih anotasi request body ke params
+    @PostMapping
+    public Product create(@RequestBody Product product) {
         return productService.save(product);
     }
 
+    //=== 2. Find All Products
+    @GetMapping
+    public Iterable<Product> findAll() {
+        return productService.findAll();
+    }
 
 }
