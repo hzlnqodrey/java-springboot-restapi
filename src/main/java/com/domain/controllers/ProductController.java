b911e5aba12ea9f0dbe94a145da7945d7ffd442f
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 17:32:17 +0700

    create ProductController, and save product

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -1,5 +1,27 @@
 package com.domain.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.domain.models.entities.Product;
+import com.domain.services.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
     
+    // Inject Product Service into Product Controller
+    // Intinya MVC
+    // Product Controller -> (memanggil) Service -> (memanggil) repository -> (memanggil) entities
+    @Autowired 
+    private ProductService productService;
+
+    // ENDPOINT PRODUCT
+
+    public Product create(Product product) {
+        return productService.save(product);
+    }
+
+
 }
