e75f6bfd9d06db55a7fd60eb46dec44c02e2baaf
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-24 15:26:31 +0700

    add id field on Product as PRIMARY KEY

--- src/main/java/com/domain/models/entities/Product.java
+++ src/main/java/com/domain/models/entities/Product.java
@@ -1,10 +1,22 @@
 package com.domain.models.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.Id; // for primary key
+import javax.persistence.GeneratedValue; // for generate autoincrement id
+import javax.persistence.GenerationType; // data type for id
 import javax.persistence.Table;
 
 @Entity // sebuah entity class harus dianotasi sebagai entity
-@Table(name = "tbl_product") // optional, but it can help naming
-public class Product {
+@Table(name = "tbl_product") // optional, but it can help naming the jpa to the mysql demoapi table product
+public class Product implements Serializable {
     
+    // DEFINISI FIELD
+
+    // PRIMARY KEY [harus anotasi memakai Id]
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
 }
